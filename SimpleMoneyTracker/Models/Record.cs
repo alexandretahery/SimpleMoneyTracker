bebb602c1f0da7680b602eb03b1bd95d4038f6e5
alexandre1 tahery <alexandre1.tahery@epitech.eu>
2023-07-24 00:01:47 +0200

    [WIP] Before to migrato to Blazor Bootstrap

---
+++ SimpleMoneyTracker/Models/Record.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleMoneyTracker.Models
+{
+    public class Record
+    {
+        public Guid Id { get; private set; }
+        public double? Amount { get; set; }
+        public string? Label { get; set; }
+        public DateTime? Date { get; set; }
+
+        public Record(double amount, string label, DateTime date)
+        {
+            Id = Guid.NewGuid();
+            Amount = amount;
+            Label = label;
+            Date = DateTime.Now;
+        }
+
+        public Record()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
