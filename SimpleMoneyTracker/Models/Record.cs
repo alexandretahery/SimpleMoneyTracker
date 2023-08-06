d32dd31cd7816a28c462f71fa167c19018d847e2
alexandre1 tahery <alexandre1.tahery@epitech.eu>
2023-08-06 22:42:50 +0200

    Btn add work

--- SimpleMoneyTracker/Models/Record.cs
+++ SimpleMoneyTracker/Models/Record.cs
@@ -3,9 +3,9 @@
     public class Record
     {
         public Guid Id { get; private set; }
-        public double? Amount { get; set; }
-        public string? Label { get; set; }
-        public DateTime? Date { get; set; }
+        public double Amount { get; set; }
+        public string Label { get; set; }
+        public DateTime Date { get; set; }
 
         public Record(double amount, string label, DateTime date)
         {
